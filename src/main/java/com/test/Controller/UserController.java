3bf839119333ea6a1f0757e56b4f3112b5076321
Abhinandan shahi <abhinandan@synarionit.com>
2024-02-09 16:45:46 +0530

    Adding Getter And Setters in USER DTO & ENTITY

--- src/main/java/com/test/Controller/UserController.java
+++ src/main/java/com/test/Controller/UserController.java
@@ -11,6 +11,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -39,5 +41,7 @@ public class UserController {
         User user = userService.detailById(userDto);
         return  new ResponseEntity<>(user,HttpStatus.FOUND);
     }
+
+
 
 }
--- src/main/java/com/test/service/UserService.java
+++ src/main/java/com/test/service/UserService.java
@@ -10,6 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
     @Autowired
@@ -43,4 +45,6 @@ public class UserService {
         User user = userRepository.findById(userDto.getId()).orElseThrow();
         return user;
     }
+
+
 }
