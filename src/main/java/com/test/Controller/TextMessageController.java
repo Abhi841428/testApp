bed6abb3616db1e5d2a41515dbe5dc30b9472e22
Abhinandan shahi <abhinandan@synarionit.com>
2024-02-13 17:44:20 +0530

    Text message Services

--- src/main/java/com/test/Controller/TextMessageController.java
+++ src/main/java/com/test/Controller/TextMessageController.java
@@ -1,20 +1,19 @@
 package com.test.Controller;
 
 
-import com.test.service.EmailService;
+import com.test.service.TextMessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-public class EmailController {
+public class TextMessageController {
 
 
     @Autowired
-    private EmailService emailService;
+    private TextMessageService textMessageService;
 
 //    @PostMapping("/sendEmail")
 //    public void sendEmail(@RequestBody EmailDTO emailDTO) {
@@ -23,7 +22,7 @@ public class EmailController {
 
     @PostMapping("/text")
     public void sendEmail() {
-        emailService.sendSms("+918709652386","Just testing");
+        textMessageService.sendSms("+918709652386","Just testing");
 
     }
 }
--- src/main/java/com/test/service/TextMessageService.java
+++ src/main/java/com/test/service/TextMessageService.java
@@ -5,18 +5,12 @@ import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 
 @Component
-public class EmailService {
+public class TextMessageService {
 
 //    @Autowired
 //    private JavaMailSender emailSender;
